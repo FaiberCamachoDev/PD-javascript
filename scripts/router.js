09450f03d329a9a7b65b42a35ea70d622de144c2
Branner Ramirez <branner360@gmail.com>
2026-02-03 21:00:14 -0500

    feat: week4 mod

--- scripts/router.js
+++ scripts/router.js
@@ -1,37 +1,60 @@
-import { getSession } from "./storage/storage.js";
+// import { getSession } from "./storage/storage.js";
 
-const publicRoutes = ["/login", "/register"];
+// const publicRoutes = ["/login", "/register"];
 
-export async function router(path) {
-  const app = document.getElementById("app");
-  const session = getSession();
+// export async function router(path) {
+//   const app = document.getElementById("app");
+//   const session = getSession();
 
-  // if not session and isn't public...
-  if (!session && !publicRoutes.includes(path)) {
-    path = "/login";
-  }
+//   // if not session and isn't public...
+//   if (!session && !publicRoutes.includes(path)) {
+//     path = "/login";
+//   }
 
-  if (session) {
-    // user denied to admin views
-    if (session.role === "user" && path === "/admin") {
-      path = "/tasks";
-    }
+//   if (session) {
+//     // user denied to admin views
+//     if (session.role === "user" && path === "/admin") {
+//       path = "/tasks";
+//     }
 
-    // Admin no view users views
-    if (session.role === "admin" && ["/tasks", "/tasks", "/profile"].includes(path)) {
-      path = "/admin";
-    }
+//     // Admin no view users views
+//     if (session.role === "admin" && ["/tasks", "/tasks", "/profile"].includes(path)) {
+//       path = "/admin";
+//     }
 
 
-  }
+//   }
 
-  const html = await fetch(`./views${path}.html`).then(r => r.text());
-  app.innerHTML = html;
-  //change routes
-  if (path === "/login") import("../scripts/controllers/login.js");
-  if (path === "/register") import("../scripts/controllers/register.js");
-  if (path === "/tasks") import("../scripts/controllers/tasks.js");
-  if (path === "/profile") import("../scripts/controllers/profile.js");
-  if (path === "/admin") import("../scripts/controllers/admin.js");
+//   const html = await fetch(`./views${path}.html`).then(r => r.text());
+//   app.innerHTML = html;
+//   //change routes
+//   if (path === "/login") import("../scripts/controllers/login.js");
+//   if (path === "/register") import("../scripts/controllers/register.js");
+//   if (path === "/tasks") import("../scripts/controllers/tasks.js");
+//   if (path === "/profile") import("../scripts/controllers/profile.js");
+//   if (path === "/admin") import("../scripts/controllers/admin.js");
 
+// }
+
+
+
+
+
+const routes = { 
+  "public" : [{
+    view : "/login",
+    styles : [],
+    scripts : []
+  }, "/register"],
+  "private" : ["/login", "/register"]
 }
+
+const roles = {
+  "admin": {
+    render : ( ) => { window.location.href = "./views/admin.html"; }
+  },
+  "user": ["/tasks", "/profile"],
+  "deafult": ["/login", "/register"]
+}
+
+roles["admin"].render();
